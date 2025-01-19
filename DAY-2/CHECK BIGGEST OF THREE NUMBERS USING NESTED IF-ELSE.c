42270cdd63c1eefcd72f940a259b533a1f6ecde2
saikatdas10 <saikatdas4101@gmail.com>
2025-01-19 21:05:52 +0530

    Create CHECK BIGGEST OF THREE NUMBERS USING NESTED IF-ELSE.c

---
+++ DAY-2/CHECK BIGGEST OF THREE NUMBERS USING NESTED IF-ELSE.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a=400,b=500,c=300;
+	if(a>b){
+		if(a>c)
+	{
+		printf("a=%d is the biggest number",a);
+	}
+	else{
+		printf("c=%d is the biggest",c);
+	}}
+	else if(b>c){
+		printf("b=%d is the biggest",b);
+	}else{
+		printf("c=%d is biggest");
+	}
+
+	return 0;
+}
