7dfd8869f1fd9bd1fc642842887a7b266d9206ef
saikatdas10 <saikatdas4101@gmail.com>
2025-01-31 14:54:11 +0530

    Create TO CREATE THIS PATTERN.c

---
+++ DAY-3/TO CREATE THIS PATTERN.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,num=1;
+for(i=1;i<=4;i++0
+  {
+for(j=1;j<=4;j++)
+{
+printf(" ");
+}
+for(j=1;j<=i;j++)
+{
+printf("%d",num);
+num++;
+}
+printf("\n");
+}
+return 0;
+}
