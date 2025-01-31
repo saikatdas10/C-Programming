39c5dec93ed8547bbebfe65ed7b9944ed6798a81
saikatdas10 <saikatdas4101@gmail.com>
2025-01-31 14:36:32 +0530

    Create TO FIND A NUMBER IS  ARMSTRONG NO OR NOT.c

---
+++ DAY-3/TO FIND A NUMBER IS  ARMSTRONG NO OR NOT.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int=153;r,rev=0,s=n;
+while(n!=0)
+{
+r=n%10;
+rev=rev+(r*r*r);
+n=n/10;
+  }
+  if(rev==s)
+  {
+    printf("The number is a armstrong no.");
+  }
+  else
+  {
+    printf("The number is not a armstrong no.");
+  }
+return 0;
+}
