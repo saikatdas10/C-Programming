e52f8368eb56d77a06d44f7f72ca93b2e2b65219
saikatdas10 <saikatdas4101@gmail.com>
2025-01-31 14:34:00 +0530

    Create FIND THE NUMBER IS PALLINDROME OR NOT.c

---
+++ DAY-3/FIND THE NUMBER IS PALLINDROME OR NOT.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int=121;r,rev=0,s=n;
+while(n!=0)
+{
+r=n%10;
+rev=rev*10+r;
+n=n/10;
+  }
+  if(rev==s)
+  {
+    printf("The number is pallindrome no.");
+  }
+  else
+  {
+    printf("The number is not a pallindrome no.");
+  }
+return 0;
+}
